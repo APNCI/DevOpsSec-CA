a6a9b61649dc2b61ad64f9b145a26f99ffb0bd1d
Alex P <56544034+AlexisPechon@users.noreply.github.com>
2023-03-10 16:54:58 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 sudo apt update && sudo apt install nodejs npm
 
+sudo npm install
+
 sudo npm install -g pm2
 
 pm2 stop example_app
 
 cd DevOpsSec-CA/
 
-sudo npm install
+
 
 pm2 start ./bin/www --name example_app
