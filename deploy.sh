eff5d1d21b8f14fd08a32e57d2f0a76cc0eba9ea
Alex P <56544034+AlexisPechon@users.noreply.github.com>
2023-03-10 16:47:06 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 sudo apt update && sudo apt install nodejs npm
 
-npm i -g pm2
+sudo npm i -g pm2
 
 pm2 stop simple_app
 
 cd DevOpsSec-CA/
 
-npm install
+sudo npm install
 
 pm2 start ./bin/www --name simple_app
