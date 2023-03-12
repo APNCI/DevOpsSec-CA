3138876be1b2f2f81ec3b8ee5f1e78696fe52e80
Alex P <56544034+AlexisPechon@users.noreply.github.com>
2023-03-12 17:21:27 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-sudo apt update && sudo apt install nodejs npm
+apt update && apt install nodejs npm
 
-sudo apt-get install npm
+apt-get install npm
 
-sudo npm install -g pm2 --verbose
+npm install -g pm2 --verbose
 
 pm2 stop example_app
 
 cd DevOpsSec-CA/
 
-sudo npm install
+npm install
 
 pm2 start ./bin/www --name example_app
